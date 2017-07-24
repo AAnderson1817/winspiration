1edd6e423a3af86f637be5c400a8f89533513c5f
Andy Anderson <andyanderson1818@gmail.com>
2017-07-24 16:06:42 -0700

    Cleaned up interface and fixed flexbox sizing. Currently functions like a screen saver

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const unsplash = 'http://www.unsplash.it';
 const photo = document.getElementById('photo');
-const display = document.getElementById('p');
-const button = document.getElementById('button');
-const ageput = document.getElementById('age');
-let width = window.innerWidth;
-let height = window.innerHeight;
-let randomNum = `${Math.floor(Math.random()*300)}`;
+const display = document.getElementById('display');
+//const button = document.getElementById('button');
+//const ageput = document.getElementById('age');
+let width = window.offsetWidth;
+let height = window.offsetHeight;
+let randomNum = `${Math.floor(Math.random()*200)}`;
 let parseNum = parseInt(getRandomNum());
 let randomPic = `${unsplash}/${width}/${height}?image=${parseNum}`;
 
 
+//Editing text functions
 
-function changeText(){
-  display.innerHTML = `I am ${ageput.value} years old`;
-}
-function clearText(){
-  ageput.value = '';
-  changeText();
-}
+//function changeText(){
+//  display.innerHTML = `I am ${ageput.value} years old`;
+//}
+//function clearText(){
+//  ageput.value = '';
+//  changeText();
+//}
+
+
+//Random photo functions
+
 function getRandomNum(){
-  let randomNum = Math.floor(Math.random()*300);
+  let randomNum = Math.floor(Math.random()*200);
   return randomNum;
 }
 function randomPhoto(){
- width = window.innerWidth;
- height = window.innerHeight;
- randomNum = `${Math.floor(Math.random()*300)}`;
+ width = document.getElementById('header').offsetWidth;
+ height = document.getElementById('header').offsetHeight;
+ randomNum = `${Math.floor(Math.random()*200)}`;
  parseNum = parseInt(getRandomNum());
  randomPic = `${unsplash}/${width}/${height}?image=${parseNum}`;
  photo.src = randomPic;
@@ -36,6 +41,10 @@ function screenCheck(){
   }
 }
 
+
+//Event listeners
+
 window.addEventListener('resize', screenCheck);
-button.addEventListener('click', randomPhoto);
-ageput.addEventListener('keyup', changeText);
+photo.addEventListener('click', randomPhoto);
+window.setInterval(randomPhoto, 5000);
+//ageput.addEventListener('keyup', changeText);
